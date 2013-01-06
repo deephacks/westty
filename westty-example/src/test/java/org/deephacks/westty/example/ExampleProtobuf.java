3ca69164eff9ff7ef436b99799ee6095e75c1a1c
Kristoffer Sjogren <stoffe@gmail.com>
2013-01-06 17:09:06 +0100

    Change example to put a little load on westty protobuf endpoint

--- westty-example/src/test/java/org/deephacks/westty/example/ExampleProtobuf.java
+++ westty-example/src/test/java/org/deephacks/westty/example/ExampleProtobuf.java
@@ -18,28 +18,34 @@ import java.net.InetSocketAddress;
 
 import org.deephacks.westty.example.CreateMessages.CreateRequest;
 import org.deephacks.westty.example.DeleteMessages.DeleteRequest;
-import org.deephacks.westty.protobuf.WesttyProtobufClient;
 import org.deephacks.westty.protobuf.ProtobufSerializer;
+import org.deephacks.westty.protobuf.WesttyProtobufClient;
 
 public class ExampleProtobuf {
     public static void main(String[] args) throws Exception {
         ProtobufSerializer serializer = new ProtobufSerializer();
         serializer.register(new File("src/main/resources/META-INF/create.desc"));
         serializer.register(new File("src/main/resources/META-INF/delete.desc"));
-        WesttyProtobufClient client = new WesttyProtobufClient(new InetSocketAddress(7777), serializer);
+        run(serializer);
+        run(serializer);
+
+    }
+
+    public static void run(ProtobufSerializer serializer) throws Exception {
+        WesttyProtobufClient client = new WesttyProtobufClient(new InetSocketAddress(7777),
+                serializer);
         client.connect();
-        CreateRequest create = CreateRequest.newBuilder().setName("name").setPassword("pw").build();
-        DeleteRequest delete = DeleteRequest.newBuilder().setName("name").build();
-        client.write(create).awaitUninterruptibly();
-        client.write(delete).awaitUninterruptibly();
-        Thread.sleep(2000);
+        for (int i = 0; i < 1000; i++) {
+            CreateRequest create = CreateRequest.newBuilder().setName("name").setPassword("pw")
+                    .build();
+            DeleteRequest delete = DeleteRequest.newBuilder().setName("name").build();
+            client.write(create).awaitUninterruptibly();
+            client.write(delete).awaitUninterruptibly();
+        }
         client.disconnect();
+
         Thread.sleep(2000);
-        client = new WesttyProtobufClient(new InetSocketAddress(7777), serializer);
-        client.connect();
-        client.write(create).awaitUninterruptibly();
-        client.write(delete).awaitUninterruptibly();
-        Thread.sleep(2000);
-        client.disconnect();
+        System.out.println("Disconnect");
+
     }
 }
