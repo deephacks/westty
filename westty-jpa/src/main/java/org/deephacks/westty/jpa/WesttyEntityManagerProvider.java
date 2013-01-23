691e4be6ffa820b61dd72234e13e5ddf1117b8b8
Kristoffer Sjogren <stoffe@gmail.com>
2013-01-24 00:31:01 +0100

    Updated entity manager handling

--- westty-jpa/src/main/java/org/deephacks/westty/jpa/WesttyEntityManagerProvider.java
+++ westty-jpa/src/main/java/org/deephacks/westty/jpa/WesttyEntityManagerProvider.java
@@ -1,75 +1,52 @@
 package org.deephacks.westty.jpa;
 
 import java.io.Serializable;
-import java.util.Stack;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
-import org.deephacks.tools4j.config.internal.core.jpa.EntityManagerProvider;
+import org.deephacks.tools4j.config.model.ThreadLocalManager;
+import org.deephacks.tools4j.config.model.ThreadLocalScope;
 
-public class WesttyEntityManagerProvider extends EntityManagerProvider implements Serializable {
+public class WesttyEntityManagerProvider extends ThreadLocalScope implements Serializable {
 
     private static final long serialVersionUID = -3337029214056469814L;
+    private static EntityManagerFactory emf;
 
-    private EntityManagerFactory emf;
-
-    private static final ThreadLocal<Stack<EntityManager>> emStackThreadLocal = new ThreadLocal<Stack<EntityManager>>();
-    private static final WesttyEntityManagerProvider instance = new WesttyEntityManagerProvider();
-
-    public static WesttyEntityManagerProvider getInstance() {
-        return instance;
-    }
-
-    public EntityManager get() {
-        final Stack<EntityManager> entityManagerStack = emStackThreadLocal.get();
-        if (entityManagerStack == null || entityManagerStack.isEmpty()) {
-            return null;
-        } else {
-            return entityManagerStack.peek();
-        }
-    }
-
-    public EntityManager createAndRegister() {
-        if (emf == null) {
-            openProvider();
-        }
-        Stack<EntityManager> entityManagerStack = emStackThreadLocal.get();
-        if (entityManagerStack == null) {
-            entityManagerStack = new Stack<EntityManager>();
-            emStackThreadLocal.set(entityManagerStack);
-        }
-
-        final EntityManager entityManager = emf.createEntityManager();
-        entityManagerStack.push(entityManager);
-        return entityManager;
-    }
-
-    public void unregister(EntityManager entityManager) {
-        final Stack<EntityManager> entityManagerStack = emStackThreadLocal.get();
-        if (entityManagerStack == null || entityManagerStack.isEmpty())
-            throw new IllegalStateException(
-                    "Removing of entity manager failed. Your entity manager was not found.");
-
-        if (entityManagerStack.peek() != entityManager)
-            throw new IllegalStateException(
-                    "Removing of entity manager failed. Your entity manager was not found.");
-        entityManagerStack.pop();
-    }
-
-    @Override
-    public EntityManager getEntityManager() {
-        return get();
-    }
-
-    @Override
-    public void openProvider() {
+    public WesttyEntityManagerProvider() {
         emf = WesttyPersistence.createEntityManagerFactory();
     }
 
-    @Override
-    public void closeProvider() {
+    public EntityManager get() {
+        return ThreadLocalManager.peek(EntityManager.class);
+    }
+
+    public EntityManager createEntityManager() {
+
+        final EntityManager em = emf.createEntityManager();
+        ThreadLocalManager.push(EntityManager.class, em);
+        return em;
+    }
+
+    public void closeEntityManagerFactory() {
         emf.close();
+    }
+
+    public void removeEntityManager() {
+        EntityManager em = ThreadLocalManager.pop(EntityManager.class);
+        if (em == null)
+            throw new IllegalStateException(
+                    "Removing of entity manager failed. Your entity manager was not found.");
+    }
+
+    @Override
+    public void createScope() {
+        createEntityManager();
+    }
+
+    @Override
+    public void closeScope() {
+        removeEntityManager();
     }
 
 }
