258f7f176d2a02864ef996c62367abe9c36304be
Kristoffer Sjogren <stoffe@gmail.com>
2013-05-05 00:04:21 +0200

    Updated whirr java 7 installation

--- westty-whirr/src/main/resources/functions/install_jre7.sh
+++ westty-whirr/src/main/resources/functions/install_jre7.sh
@@ -2,43 +2,45 @@
 
 function install_jre7() {
 
-  target_dir=/usr/lib/jvm/java-7-oracle
+  jdk_file=jdk-7u21-linux-x64.tar.gz
+  jdk_version=jdk1.7.0_21
 
-  if [ -e "$target_dir" ]; then
-    echo "It appears java is already installed. Skipping java installation."
-    echo "Move $target_dir out of the way if you want to reinstall"
-    #nasty little hack... somewhere the string 'r e t u r n' gets replaced by exit
-    turn=turn
-    re$turn
-  fi
+  target_dir=/usr/lib/jvm
 
   tmpdir=`mktemp -d`
 
-  curl http://javadl.sun.com/webapps/download/AutoDL?BundleId=75252 -L --silent --show-error --fail --connect-timeout 60 --max-time 600 --retry 5 -o $tmpdir/java.tar.gz
+  curl https://deephacks.googlecode.com/files/jdk-7u21-linux-x64.gz -L --show-error --fail --connect-timeout 60 --max-time 600 --retry 5 -o $tmpdir/java.tar.gz
 
   (cd $tmpdir; tar xzf java.tar.gz)
-  mkdir -p `dirname $target_dir`
-  (cd $tmpdir; mv jre* $target_dir)
+  rm -rf $tmpdir/java.tar.gz
+  mkdir -p $target_dir
+  (cd $tmpdir; mv * $target_dir)
   rm -rf $tmpdir
 
-  update-alternatives --install /usr/bin/java java $target_dir/bin/java 17000
-  update-alternatives --set java $target_dir/bin/java
+  update-alternatives --install /usr/bin/java java $target_dir/$jdk_version/bin/java 17000
+  update-alternatives --set java $target_dir/$jdk_version/bin/java
 
   # Try to set JAVA_HOME in a number of commonly used locations
-  export JAVA_HOME=$target_dir
+  export JAVA_HOME=$target_dir/$jdk_version
+  export PATH=$JAVA_HOME/bin:$PATH
   if [ -f /etc/profile ]; then
     echo export JAVA_HOME=$JAVA_HOME >> /etc/profile
+    export PATH=$JAVA_HOME/bin:$PATH >> /etc/profile
   fi
   if [ -f /etc/bashrc ]; then
     echo export JAVA_HOME=$JAVA_HOME >> /etc/bashrc
+    echo export PATH=$JAVA_HOME/bin:$PATH >> /etc/bashrc
   fi
   if [ -f ~root/.bashrc ]; then
     echo export JAVA_HOME=$JAVA_HOME >> ~root/.bashrc
+    echo export PATH=$JAVA_HOME/bin:$PATH >> ~root/.bashrc
   fi
   if [ -f /etc/skel/.bashrc ]; then
     echo export JAVA_HOME=$JAVA_HOME >> /etc/skel/.bashrc
+    echo export PATH=$JAVA_HOME/bin:$PATH >> /etc/skel/.bashrc
   fi
   if [ -f "$DEFAULT_HOME/$NEW_USER" ]; then
     echo export JAVA_HOME=$JAVA_HOME >> $DEFAULT_HOME/$NEW_USER
+    echo export PATH=$JAVA_HOME/bin:$PATH >> $DEFAULT_HOME/$NEW_USER
   fi
 }
